3ec4a89f5759cc79f33f319df6a5f46d7e3cb600
Ivan Gerasimow <ivangerasimow@gmail.com>
2015-07-29 20:59:27 +0400

    Bugfixies and major updates
    Added description page for each item in catalogue

---
+++ src/php/itemsRouter.php
@@ -0,0 +1,28 @@
+  <?php
+    $catalogue_filePath = '../json/items.json';
+    $catalogue = json_decode(file_get_contents($catalogue_filePath));
+    $id = $_POST['itemId'];
+
+    foreach($catalogue as $category) {
+      foreach($category->{'items'} as $item) {
+        if (strcmp($id, $item->{'id'}) === 0)
+          break 2;
+      }
+    }
+
+    echo json_encode(array("id"=>$item->{'id'},
+                          "title"=>$item->{'title'},
+                          "age"=>$item->{'age'},
+                          "description"=>$item->{'description'},
+                          "price1"=>$item->{'price1'},
+                          "price2"=>$item->{'price2'},
+                          "price3"=>$item->{'price3'},
+                          "available"=>$item->{'available'},
+                          "date"=>$item->{'date'}));
+
+    unset($catalogue_filePath);
+    unset($catalogue);
+    unset($id);
+    unset($category);
+    unset($item);
+  ?>
