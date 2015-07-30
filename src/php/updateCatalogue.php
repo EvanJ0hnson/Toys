ce590e578c954ab6bf75d437edaf80687c2afa20
Ivan Gerasimow <ivangerasimow@gmail.com>
2015-07-30 16:33:37 +0400

    Added administration page

---
+++ src/php/updateCatalogue.php
@@ -0,0 +1,27 @@
+<?php
+  header('Content-Type: text/html; charset=utf-8');
+  // $message = $_REQUEST['message'];
+  // $id = $_POST['json'];
+  // if (strlen($message) == 0) {
+  //   echo 'error::empty';
+  //   exit;
+  // }
+
+  // $newItems = explode(',', $_REQUEST['message']);
+
+  $newItems = $_POST['json'];
+
+  $catalogue_filePath = '../json/items.json';
+  // $calendar = json_decode(file_get_contents($catalogue_filePath));
+
+  // foreach ($newItems as $item) {
+  //   $calendar[] = $item;
+  // }
+
+  $fp = fopen($catalogue_filePath, 'w');
+  flock ($fp, LOCK_EX);
+  fwrite($fp, json_encode($newItems));
+  flock($fp, LOCK_UN);
+  fclose($fp);
+  echo 'Выполнено';
+?>
